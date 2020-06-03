5a7ef8ca14276c4ef75aa60f46faee5859f8bccc
rohanp <rohanpanu98@gmail.com>
2020-06-03 10:15:28 -0700

    Typewriter Effect

--- index.js
+++ index.js
@@ -1,3 +1,60 @@
-function openResume(){
+function openResume() {
     window.open('RohanPanugantiResume.pdf');
 }
+
+var typewrite = function (el, rotate, period) {
+    this.rotate = rotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+};
+
+typewrite.prototype.tick = function () {
+    var i = this.loopNum % this.rotate.length;
+    var fullTxt = this.rotate[i];
+
+    if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
+
+    var that = this;
+    var delta = 200 - Math.random() * 100;
+
+    if (this.isDeleting) { delta /= 2; }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period;
+        this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
+    }
+
+    setTimeout(function () {
+        that.tick();
+    }, delta);
+};
+
+window.onload = function () {
+    var elements = document.getElementsByClassName('typewrite');
+    for (var i = 0; i < elements.length; i++) {
+        var rotate = elements[i].getAttribute('data-type');
+        var period = elements[i].getAttribute('data-period');
+        if (rotate) {
+            new typewrite(elements[i], JSON.parse(rotate), period);
+        }
+    }
+
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+    document.body.appendChild(css);
+};
