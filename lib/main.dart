ff89180f83882339ca18528a4349fb504dc52ed6
Rohan Panuganti <panugant@seattleu.edu>
2020-08-07 12:22:00 -0700

    Start convert to Flutter

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(fontFamily: 'Monospace', backgroundColor: Colors.white),
+      title: "Rohan Panuganti",
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0.0,
+        leading: Image.asset(
+          "images/rp.svg",
+          height: 30,
+        ),
+      ),
+    );
+  }
+}
