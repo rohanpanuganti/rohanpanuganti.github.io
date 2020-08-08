80882847fee66093ae568adb187db445e749ff52
Rohan Panuganti <panugant@seattleu.edu>
2020-08-08 02:28:44 -0700

    Top and Bottom Bar are complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+// ignore: avoid_web_libraries_in_flutter
+import 'dart:html' as html;
+
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,6 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(fontFamily: 'Monospace', backgroundColor: Colors.white),
+      debugShowCheckedModeBanner: false,
       title: "Rohan Panuganti",
       home: Home(),
     );
@@ -21,15 +26,68 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  List<bool> hover = [false, false, false];
+
+  Widget boop(IconData icon, int i, String url) {
+    return Padding(
+        padding: EdgeInsets.only(
+          left: 20,
+          bottom: 20,
+        ),
+        child: InkWell(
+          hoverColor: Colors.black,
+          onTap: () async {
+            html.window.open(url, 'new tab');
+          },
+          child: Container(
+              height: 40,
+              width: 40,
+              child: Center(
+                  child: FaIcon(
+                icon,
+                size: 35,
+                color: hover[i] ? Colors.white : Colors.black,
+              ))),
+          onHover: (h) async {
+            setState(() {
+              hover[i] = h;
+            });
+          },
+        ));
+  }
+
+  Widget text() {
+    return null;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
+        toolbarHeight: 70,
         backgroundColor: Colors.white,
+        centerTitle: false,
         elevation: 0.0,
-        leading: Image.asset(
-          "images/rp.svg",
-          height: 30,
+        title: Image.asset(
+          "images/rp.png",
+          height: 50,
+          alignment: Alignment.topLeft,
+        ),
+      ),
+      body: null,
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.white,
+        elevation: 0.0,
+        child: Row(
+          children: [
+            boop(FontAwesomeIcons.github, 0,
+                "https://github.com/rohanpanuganti/"),
+            boop(FontAwesomeIcons.linkedin, 1,
+                "https://www.linkedin.com/in/rohanpanuganti/"),
+            boop(FontAwesomeIcons.filePdf, 2,
+                "https://nbviewer.jupyter.org/github/rohanpanuganti/Resume-New/blob/master/Resume.pdf"),
+          ],
         ),
       ),
     );
