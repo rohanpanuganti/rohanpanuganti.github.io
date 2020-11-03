a45c743c4e9f55edcd5feacb26ff618446cea35e
Rohan Panuganti <panugant@seattleu.edu>
2020-11-03 01:51:40 -0800

    fix music

--- lib/main.dart
+++ lib/main.dart
@@ -148,7 +148,6 @@ class _HomeState extends State<Home> {
                 ],
               ),
               Container(height: 50),
-              /*
               GestureDetector(
                   behavior: HitTestBehavior.opaque,
                   onTap: () async {
@@ -166,13 +165,11 @@ class _HomeState extends State<Home> {
                           child: _iframeWidget),
                     ],
                   )),
-                
               Text(
                 '^^ Click Me! ^^',
                 style: GoogleFonts.robotoMono(fontSize: 25),
               ),
               Container(height: 50),
-               */
               Container(
                   padding: EdgeInsets.only(left: 20),
                   alignment: Alignment.centerLeft,
