9af081e4f511dba21c4c6f1516f3309cce4ebcc3
Rohan Panuganti <panugant@seattleu.edu>
2020-12-16 10:26:33 -0800

    minor fix

--- lib/main.dart
+++ lib/main.dart
@@ -256,7 +256,7 @@ class _MainBodyState extends State<MainBody>
                 text: "whoami",
                 onTap: () => drawerKey.currentState.openDrawer(),
               ),
-              Container(width: 10),
+              Text('  '),
             ],
             backgroundColor: Colors.transparent,
             elevation: 0,
